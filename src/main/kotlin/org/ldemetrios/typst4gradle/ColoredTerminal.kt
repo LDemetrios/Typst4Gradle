d26445d60c0e900ec15d6bc864e153ce70491fe8
Dmitry Liapin <ldemetrios@yandex.ru>
2024-02-27 15:31:12 +0300

    Handle IOException in ColoredTerminal.kt.execColored

--- src/main/kotlin/org/ldemetrios/typst4gradle/ColoredTerminal.kt
+++ src/main/kotlin/org/ldemetrios/typst4gradle/ColoredTerminal.kt
@@ -2,39 +2,44 @@ package org.ldemetrios.typst4gradle
 
 import org.ldemetrios.utilities.constants.NEWLINES
 import java.io.File
+import java.io.IOException
 
 fun execColored(command: List<String>, root: File? = null): ColoredExecResult {
-    val builder = ProcessBuilder(command)
-    if (root != null) builder.directory(root)
-    val process = builder.start()
+    try {
+        val builder = ProcessBuilder(command)
+        if (root != null) builder.directory(root)
+        val process = builder.start()
 
-    process.outputStream.close()
+        process.outputStream.close()
 
-    val out = process.inputStream
-    val err = process.errorStream
+        val out = process.inputStream
+        val err = process.errorStream
 
-    val marked = mutableListOf<Pair<String, String>>()
+        val marked = mutableListOf<Pair<String, String>>()
 
-    val outWriter = LinedOutput { marked.add("output" to it) }
-    val errWriter = LinedOutput { marked.add("error" to it) }
+        val outWriter = LinedOutput { marked.add("output" to it) }
+        val errWriter = LinedOutput { marked.add("error" to it) }
 
-    do {
-        var any = false
+        do {
+            var any = false
 
-        val outChar = out.read()
-        if (outChar != -1) {
-            outWriter.write(outChar)
-            any = true
-        }
+            val outChar = out.read()
+            if (outChar != -1) {
+                outWriter.write(outChar)
+                any = true
+            }
 
-        val errChar = err.read()
-        if (errChar != -1) {
-            errWriter.write(errChar)
-            any = true
-        }
-    } while (any)
+            val errChar = err.read()
+            if (errChar != -1) {
+                errWriter.write(errChar)
+                any = true
+            }
+        } while (any)
 
-    return ColoredExecResult(marked, process.waitFor())
+        return ColoredExecResult(marked, process.waitFor())
+    } catch (e: IOException) {
+        return ColoredExecResult(listOf(), -1)
+    }
 }
 
 private const val CR_CODE = '\r'.code
@@ -75,4 +80,4 @@ class LinedOutput(val onLine: (String) -> Unit) {
     }
 }
 
-data class ColoredExecResult(val marked: MutableList<Pair<String, String>>, val code: Int)
+data class ColoredExecResult(val marked: List<Pair<String, String>>, val code: Int)
